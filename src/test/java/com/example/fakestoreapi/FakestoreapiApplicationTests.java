83ca6dccb026086b2a7d16731356c77f124fd5df
vaibhav Kshirsagar <vaibhavKshirsagar8@gmail.com>
2025-08-16 22:45:44 +0530

    first commit

---
+++ src/test/java/com/example/fakestoreapi/FakestoreapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fakestoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakestoreapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
