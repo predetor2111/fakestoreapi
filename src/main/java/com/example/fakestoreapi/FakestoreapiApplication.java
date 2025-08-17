03563eac4899bec5ca2d082095071b43f217b583
predator2111 <vaibhavKshirsagar8@gmail.com>
2025-08-17 12:00:05 +0530

    added .env file

--- src/main/java/com/example/fakestoreapi/FakestoreapiApplication.java
+++ src/main/java/com/example/fakestoreapi/FakestoreapiApplication.java
@@ -1,12 +1,16 @@
 package com.example.fakestoreapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FakestoreapiApplication {
 
+
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(dotenvEntry -> System.setProperty(dotenvEntry.getKey(), dotenvEntry.getValue()));
 		SpringApplication.run(FakestoreapiApplication.class, args);
 	}
 
