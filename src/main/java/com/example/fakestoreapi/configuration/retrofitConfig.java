e8b261d26edea19bd537708474d9d6acb175890d
predator2111 <vaibhavKshirsagar8@gmail.com>
2025-08-25 09:24:44 +0530

    encoded base url in .env file

--- src/main/java/com/example/fakestoreapi/configuration/retrofitConfig.java
+++ src/main/java/com/example/fakestoreapi/configuration/retrofitConfig.java
@@ -1,18 +1,24 @@
 package com.example.fakestoreapi.configuration;
 
 import com.example.fakestoreapi.gateway.api.FakeStoreCategoryResponseApi;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
 
 @Configuration
 public class retrofitConfig {
 
     @Bean
     public Retrofit retrofit(){
+
+        Dotenv dotenv = Dotenv.load();
+        String baseUrl = dotenv.get("BASE_URL");
+
         return new Retrofit.Builder()
-                .baseUrl("https://fakestoreapi.in/api/")
+                .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
